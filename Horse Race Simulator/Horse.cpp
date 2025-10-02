3367e7bde96083f948d0b76ae2b619049e122ee1
theboss3256 <tommybrewin3256@gmail.com>
2025-10-02 15:16:36 +0100

    Added horse class to horse.cpp

--- Horse Race Simulator/Horse.cpp
+++ Horse Race Simulator/Horse.cpp
@@ -1,1 +1,5 @@
 #include "Horse.h"
+#include <iostream>
+using namespace std;
+
+Horse::Horse(string n, float spd, float acc, float sta, int cond, RunningStyle style, TrackType t, float hand) : name(n), speed(spd), acceleration(acc), stamina(sta), condition(cond), runningStyle(style), track(t), handling(hand) {}
